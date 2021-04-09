2210103859b84c475d36a4818a3fda408e09bcaa
Daniel Meszaros <easimer@gmail.com>
2021-04-09 22:54:36 +0200

    Add tests for the action system

--- actions.c
+++ actions.c
@@ -11,6 +11,7 @@
 typedef struct actions_button {
     uint8_t used : 1;
     uint8_t held : 1;
+    uint8_t pending : 1;
 
     uint16_t millisHeld;
     uint16_t millisLastRelease;
@@ -40,9 +41,12 @@ actions_status_t actionsCreateButton(actions_button_handle_t *handle, void *user
         if(!buttons[i].used) {
             buttons[i].used = 1;
             buttons[i].held = 0;
+            buttons[i].pending = 0;
             buttons[i].millisHeld = 0;
+            buttons[i].millisLastRelease = 65535;
             buttons[i].user = user;
             buttons[i].descriptor = descriptor;
+            *handle = i;
             return EACTIONS_OK;
         }
     }
@@ -73,10 +77,11 @@ actions_status_t actionsSetButtonState(actions_button_handle_t handle, uint8_t i
     if(!btn->held && isHeld) {
         btn->millisHeld = 0;
         
-        if(btn->millisLastRelease < ACTIONS_DOUBLE_CLICK_MAX_MILLISECONDS_SINCE_LAST_RELEASE) {
+        if(btn->pending && btn->millisLastRelease < ACTIONS_DOUBLE_CLICK_MAX_MILLISECONDS_SINCE_LAST_RELEASE) {
             INVOKE_CALLBACK_IF_NOT_NULL(btn->descriptor, doubleClick, handle, btn->user);
             btn->millisLastRelease = 0;
         } else {
+            btn->pending = 1;
             INVOKE_CALLBACK_IF_NOT_NULL(btn->descriptor, pressed, handle, btn->user);
         }
     } else if(btn->held && !isHeld) {
@@ -85,8 +90,7 @@ actions_status_t actionsSetButtonState(actions_button_handle_t handle, uint8_t i
 
         if(btn->millisHeld >= ACTIONS_LONG_PRESS_MIN_MILLISECONDS) {
             INVOKE_CALLBACK_IF_NOT_NULL(btn->descriptor, longPress, handle, btn->user);
-        } else {
-            INVOKE_CALLBACK_IF_NOT_NULL(btn->descriptor, click, handle, btn->user);
+            btn->pending = 0;
         }
 
         btn->millisHeld = 0;
@@ -98,16 +102,23 @@ actions_status_t actionsSetButtonState(actions_button_handle_t handle, uint8_t i
 }
 
 actions_status_t actionsTimeElapsed(uint8_t millisElapsed) {
-    for (uint8_t i = 0; i < ACTIONS_MAX_BUTTONS; i++) {
-        actions_button_t *button = &buttons[i];
+    for (uint8_t handle = 0; handle < ACTIONS_MAX_BUTTONS; handle++) {
+        actions_button_t *button = &buttons[handle];
         if (button->used) {
-            INVOKE_CALLBACK_IF_NOT_NULL(button->descriptor, probe, i, button->user);
+            INVOKE_CALLBACK_IF_NOT_NULL(button->descriptor, probe, handle, button->user);
 
             if (button->held) {
                 button->millisHeld += millisElapsed;
             } else {
                 button->millisLastRelease += millisElapsed;
             }
+
+            if (!button->held && button->pending && button->millisLastRelease > ACTIONS_DOUBLE_CLICK_MAX_MILLISECONDS_SINCE_LAST_RELEASE) {
+                INVOKE_CALLBACK_IF_NOT_NULL(button->descriptor, click, handle, button->user);
+                button->pending = 0;
+            }
         }
     }
+
+    return EACTIONS_OK;
 }
---
+++ tests/actions.tests.c
@@ -0,0 +1,169 @@
+#include "utest.h"
+
+#include "config.h"
+#include "actions.h"
+
+void testPress(actions_button_handle_t handle, void *user);
+void testRelease(actions_button_handle_t handle, void *user);
+void testClick(actions_button_handle_t handle, void *user);
+void testLongPress(actions_button_handle_t handle, void *user);
+void testDoubleClick(actions_button_handle_t handle, void *user);
+
+static actions_button_descriptor_t const testDescriptor = {
+    .probe = NULL,
+
+    .pressed = testPress,
+    .released = testRelease,
+    .click = testClick,
+    .longPress = testLongPress,
+    .doubleClick = testDoubleClick 
+};
+
+typedef struct Actions {
+    actions_button_handle_t button;
+
+    int pressed;
+    int released;
+    int click;
+    int longPress;
+    int doubleClick;
+} actions_state_t;
+
+UTEST_F_SETUP(Actions) {
+    actions_status_t rc;
+    
+    utest_fixture->pressed = 0;
+    utest_fixture->released = 0;
+    utest_fixture->click = 0;
+    utest_fixture->longPress = 0;
+    utest_fixture->doubleClick = 0;
+
+    rc = actionsCreateButton(&utest_fixture->button, utest_fixture, &testDescriptor);
+    ASSERT_EQ(rc, EACTIONS_OK);
+}
+
+UTEST_F_TEARDOWN(Actions) {
+    actions_status_t rc;
+    
+    rc = actionsDestroyButton(utest_fixture->button);
+    ASSERT_EQ(rc, EACTIONS_OK);
+}
+
+UTEST_F(Actions, CreateButton) {
+    actions_button_handle_t handle;
+    actions_status_t rc;
+    actions_button_descriptor_t d = {
+        .probe = NULL,
+
+        .pressed = NULL,
+        .released = NULL,
+        .click = NULL,
+        .longPress = NULL,
+        .doubleClick = NULL
+    };
+
+    rc = actionsCreateButton(&handle, NULL, &d);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    
+    actionsDestroyButton(handle);
+}
+
+#define ASSERT_ACTIONS_STATE(state, expectedPressed, expectedReleased, expectedClick, expectedLongPress, expectedDoubleClick)   \
+    ASSERT_EQ(state->pressed, expectedPressed);													                                \
+    ASSERT_EQ(state->released, expectedReleased);												                                \
+    ASSERT_EQ(state->click, expectedClick);														                                \
+    ASSERT_EQ(state->longPress, expectedLongPress);												                                \
+    ASSERT_EQ(state->doubleClick, expectedDoubleClick);
+
+UTEST_F(Actions, Pressed) {
+    actions_status_t rc;
+
+    rc = actionsSetButtonState(utest_fixture->button, 1);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    ASSERT_EQ(utest_fixture->pressed, 1);
+}
+
+UTEST_F(Actions, Released) {
+    actions_status_t rc;
+
+    rc = actionsSetButtonState(utest_fixture->button, 1);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsSetButtonState(utest_fixture->button, 0);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    ASSERT_EQ(utest_fixture->released, 1);
+}
+
+UTEST_F(Actions, Click) {
+    actions_status_t rc;
+
+    rc = actionsSetButtonState(utest_fixture->button, 1);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsTimeElapsed(10);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsSetButtonState(utest_fixture->button, 0);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsTimeElapsed(ACTIONS_DOUBLE_CLICK_MAX_MILLISECONDS_SINCE_LAST_RELEASE + 1);
+    ASSERT_EQ(rc, EACTIONS_OK);
+
+    ASSERT_ACTIONS_STATE(utest_fixture, 1, 1, 1, 0, 0);
+}
+
+UTEST_F(Actions, LongPress) {
+    actions_status_t rc;
+
+    rc = actionsSetButtonState(utest_fixture->button, 1);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsTimeElapsed(ACTIONS_LONG_PRESS_MIN_MILLISECONDS);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsSetButtonState(utest_fixture->button, 0);
+    ASSERT_EQ(rc, EACTIONS_OK);
+
+    ASSERT_ACTIONS_STATE(utest_fixture, 1, 1, 0, 1, 0);
+}
+
+UTEST_F(Actions, DoubleClick) {
+    actions_status_t rc;
+
+    rc = actionsSetButtonState(utest_fixture->button, 1);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsTimeElapsed(10);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsSetButtonState(utest_fixture->button, 0);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsTimeElapsed(ACTIONS_DOUBLE_CLICK_MAX_MILLISECONDS_SINCE_LAST_RELEASE / 2);
+    ASSERT_EQ(rc, EACTIONS_OK);
+
+    rc = actionsSetButtonState(utest_fixture->button, 1);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsTimeElapsed(10);
+    ASSERT_EQ(rc, EACTIONS_OK);
+    rc = actionsSetButtonState(utest_fixture->button, 0);
+    ASSERT_EQ(rc, EACTIONS_OK);
+
+    ASSERT_ACTIONS_STATE(utest_fixture, 1, 1, 0, 0, 1);
+}
+
+void testPress(actions_button_handle_t handle, void *user) {
+    actions_state_t *state = (actions_state_t *)user;
+    state->pressed = 1;
+}
+
+void testRelease(actions_button_handle_t handle, void *user) {
+    actions_state_t *state = (actions_state_t *)user;
+    state->released = 1;
+}
+
+void testClick(actions_button_handle_t handle, void *user) {
+    actions_state_t *state = (actions_state_t *)user;
+    state->click = 1;
+}
+
+void testLongPress(actions_button_handle_t handle, void *user) {
+    actions_state_t *state = (actions_state_t *)user;
+    state->longPress = 1;
+}
+
+void testDoubleClick(actions_button_handle_t handle, void *user) {
+    actions_state_t *state = (actions_state_t *)user;
+    state->doubleClick = 1;
+}
