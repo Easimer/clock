6f5364d88a4a973b68a86dfbdd7a650f68f91168
Daniel Meszaros <easimer@gmail.com>
2021-04-11 21:17:09 +0200

    run_tests: replace pushd's with cd's

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -12,22 +12,19 @@ PATH_BUILD="$WORKSPACE/out"
 PATH_GCOV="$PATH_BUILD/Testing/CoverageInfo"
 PATH_COVERAGE_XML="$PATH_BUILD/coverage.xml"
 
-pushd $WORKSPACE
+cd $WORKSPACE
 
 rm -rf $PATH_BUILD
 mkdir -p $PATH_BUILD
 
-pushd $PATH_BUILD
+cd $PATH_BUILD
 cmake -DCMAKE_BUILD_TYPE=Debug "$WORKSPACE"
-popd
+cd $PATH_SOURCE
 
 ctest -S test.ctest
 RESULT_CODE=$?
 
-pushd "$PATH_GCOV"
+cd "$PATH_GCOV"
 gcovr -r "$PATH_SOURCE" -x "$PATH_COVERAGE_XML"
-popd
-
-popd
 
 exit $RESULT_CODE
