01c9441b2519e2f29db63903877e66d5818caa54
Daniel Meszaros <easimer@gmail.com>
2021-04-15 21:19:20 +0200

    Add test Core.SetMinuteButton

--- tests/core.tests.c
+++ tests/core.tests.c
@@ -23,6 +23,7 @@ static display_hardware_status_t showTime(void *user, uint8_t hour, uint8_t minu
 static display_hardware_status_t showIcon(void *user, display_icon_t icon);
 
 UTEST_F_SETUP(Core) {
+    int rc;
     utest_fixture->coreState.buttonProbeUser = utest_fixture;
     utest_fixture->coreState.buttonProbe = coreButtonProbe;
     utest_fixture->coreState.externalMemory = &utest_fixture->memoryAccess;
@@ -35,7 +36,9 @@ UTEST_F_SETUP(Core) {
     utest_fixture->display.user = utest_fixture;
     utest_fixture->coreState.display = &utest_fixture->display;
 
-    coreInit(&utest_fixture->coreState);
+    rc = coreInit(&utest_fixture->coreState);
+    ASSERT_EQ(rc, 0);
+    timerEnable(TIMER_ID1);
 }
 
 UTEST_F_TEARDOWN(Core) {
@@ -46,6 +49,31 @@ UTEST_F(Core, InitialClockTime) {
     ASSERT_EQ(utest_fixture->displayMinute, 0);
     ASSERT_EQ(utest_fixture->displaySecond, 0);
     ASSERT_EQ(utest_fixture->displayIcon, EDISPICON_CLOCK);
+}
+
+UTEST_F(Core, SetMinuteButton) {
+    int rc;
+    uint8_t hour, minute, second;
+    timekeeper_t *tkClock;
+
+    tkClock = coreGetClock(&utest_fixture->coreState);
+
+    // Press the `set time` button five times
+    for (int i = 0; i < 5; i++) {
+        utest_fixture->buttonStates[3] = 1;
+        rc = coreElapsed(&utest_fixture->coreState, 25);
+        utest_fixture->buttonStates[3] = 0;
+        rc = coreElapsed(&utest_fixture->coreState, 1);
+
+        for (int i = 0; i < ACTIONS_DOUBLE_CLICK_MAX_MILLISECONDS_SINCE_LAST_RELEASE; i++) {
+            rc = coreElapsed(&utest_fixture->coreState, 1);
+        }
+    }
+
+    timekeeperGet(tkClock, &hour, &minute, &second);
+    ASSERT_EQ(hour, 0);
+    ASSERT_EQ(minute, 5);
+    ASSERT_EQ(second, 0);
 }
 
 static uint8_t coreButtonProbe(void *user, uint8_t btnIdx) {
